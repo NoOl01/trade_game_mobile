53ec107d9cf23e7bbfcc05ae09120e4056475ccc
NoOl01 <tagaevanton0@gmail.com>
2025-03-31 17:46:30 +0200

    gradle fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.okhttp)
+    implementation(libs.androidx.core.i18n)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/trade_game/presenter/StockScreen.kt
+++ app/src/main/java/com/example/trade_game/presenter/StockScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,9 +19,13 @@ import androidx.navigation.NavController
 import com.example.trade_game.domain.view.MarketViewModel
 import com.jaikeerthick.composable_graphs.composables.line.LineGraph
 import com.jaikeerthick.composable_graphs.composables.line.model.LineData
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun StockScreen(assetId: Int, navController: NavController, isGestureNavigation: Boolean, viewModel: MarketViewModel = viewModel()){
+    val marketHistory by viewModel.marketHistory.collectAsState()
 
     LaunchedEffect(Unit) {
         viewModel.getPriceHistory(assetId)
@@ -36,12 +42,26 @@ fun StockScreen(assetId: Int, navController: NavController, isGestureNavigation:
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            val data = listOf(LineData(x = "Sun", y = 200), LineData(x = "Mon", y = 40))
-            LineGraph(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp, vertical = 12.dp),
-                data = data,
-            )
+            marketHistory?.data?.let { history ->
+
+
+                val chartData = history.map { priceData ->
+                    val instant = Instant.ofEpochSecond(priceData.timestamp.toLong())
+                    val localDateTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime()
+                    val formated = DateTimeFormatter.ofPattern("HH:mm:ss")
+
+                    LineData(
+                        x = localDateTime.format(formated),
+                        y = priceData.price.toFloat()
+                    )
+                }
+
+                LineGraph(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 12.dp),
+                    data = chartData,
+                )
+            }
         }
     }
 }
